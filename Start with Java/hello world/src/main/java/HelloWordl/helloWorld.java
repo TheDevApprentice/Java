1d8a538187afef203cc7109922d5f2551a1dd7f5
Hugo Abric <80226527+TheDevApprentice@users.noreply.github.com>
2021-12-20 10:20:00 -0500

    .code

--- Start with Java/hello world/src/main/java/HelloWordl/helloWorld.java
+++ Start with Java/hello world/src/main/java/HelloWordl/helloWorld.java
@@ -2,7 +2,6 @@ package HelloWordl;
 
 public class helloWorld {
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello;;: World");
+        System.out.println("Hello: World");
     }
 }
